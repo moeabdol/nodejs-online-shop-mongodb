b82308edc19ab016200fc94ad56faf5db20a8055
moeabdol <mohd.a.saed@gmail.com>
2018-11-11 17:09:18 +0300

    Restructure views directory

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 const getAddProduct = (req, res) => {
-  res.render('add-product', {
+  res.render('admin/add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     activeAddProduct: true,
@@ -18,7 +18,7 @@ const postAddProduct = (req, res) => {
 
 const getProducts = (req, res) => {
   Product.fetchAll(products => {
-    res.render('shop', {
+    res.render('shop/product-list', {
       pageTitle: 'Shop',
       prods: products,
       path: '/',
