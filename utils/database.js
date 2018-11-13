22bed100f5c5f7ae72ab89aa29dc2ab516ecddbe
moeabdol <mohd.a.saed@gmail.com>
2018-11-13 20:18:08 +0300

    Sequelize operatorsAliases false to not show deprecation warning

--- utils/database.js
+++ utils/database.js
@@ -3,7 +3,8 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize('online_shop', 'moeabdol', '12345678', {
   host: 'localhost',
   dialect: 'mysql',
-  logging: false
+  logging: false,
+  operatorsAliases: false
 });
 
 module.exports = sequelize;
