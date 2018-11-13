66f0016e6389ca1f7a5ed56eaad256edfebe82bc
moeabdol <mohd.a.saed@gmail.com>
2018-11-13 20:15:38 +0300

    Disable sequelize logging

--- utils/database.js
+++ utils/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('online_shop', 'moeabdol', '12345678', {
   host: 'localhost',
-  dialect: 'mysql'
+  dialect: 'mysql',
+  logging: false
 });
 
 module.exports = sequelize;
